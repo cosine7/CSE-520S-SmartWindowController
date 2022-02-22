7234dd4a192624d34de285f99dee342693eb663f
ChenXiao Liu <810196813@qq.com>
2022-02-22 17:17:44 -0600

    Initial Commit

---
+++ SmartWindowController/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartWindowController
+//
+//  Created by lcx on 2022/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartWindowController/SmartWindowControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartWindowControllerApp.swift
+//  SmartWindowController
+//
+//  Created by lcx on 2022/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SmartWindowControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
